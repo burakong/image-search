96847b3f0404b45bc38667802661b7774a2f5167
burakong <burakong44@outlook.de>
2023-04-26 11:16:23 +0200

    create the image search component with the cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageSearchComponent } from './image-search/image-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSearchComponent
   ],
   imports: [
     BrowserModule,
