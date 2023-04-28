fd65eeae9e4c64c26017c7dc28d336101c9994f0
burakong <burakong44@outlook.de>
2023-04-28 09:58:58 +0200

    create new component called imageDetail to displaying all the details of an image.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { ImageSearchComponent } from './image-search/image-search.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageSearchComponent
+    ImageSearchComponent,
+    ImageDetailComponent
   ],
   imports: [
     BrowserModule,
