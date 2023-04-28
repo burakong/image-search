bd5711670978efac85ff24ae6f87489a926d35fe
burakong <burakong44@outlook.de>
2023-04-28 10:12:24 +0200

    refactor subscribtion

--- src/app/image-search/image-search.component.ts
+++ src/app/image-search/image-search.component.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { PhotoSearchService } from '../photo-search.service';
+import { Subject, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-image-search',
   templateUrl: './image-search.component.html',
   styleUrls: ['./image-search.component.scss']
 })
-export class ImageSearchComponent {
+export class ImageSearchComponent implements OnDestroy {
 
-  searchData: string = "";
-  perPage: number = 6;
-  photos: any = [];
+  public searchData: string = "";
+  public perPage: number = 6;
+  public photos: any = [];
+  private _unsubscribe$: Subject<void> = new Subject();
   constructor(private photoSearchService: PhotoSearchService) { }
 
   search() {
-    this.photoSearchService.getdata(this.searchData, this.perPage).subscribe((response: any) => {
-      console.log(response.photos);
-      this.photos = response.photos;
-    }, (error: Error) => {
-      console.log(error);
-    })
+    this.photoSearchService.getdata(this.searchData, this.perPage).pipe(take(1),
+      takeUntil(this._unsubscribe$),
+      tap((response: any) => {
+        console.log(response.photos);
+        this.photos = response.photos;
+      }))
+      .subscribe();
   }
 
+  ngOnDestroy() {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
+  }
 }
