0a2f3a9ef5fb15f985e04a006c1bdb718284f022
burakong <burakong44@outlook.de>
2023-04-28 10:01:52 +0200

    add routes to the router array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImageSearchComponent } from './image-search/image-search.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'image-search', component: ImageSearchComponent },
+  { path: 'image-detail', component: ImageDetailComponent },
+  { path: '', redirectTo: '/image-search', pathMatch: 'full' }, // redirect to `first-component`
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
