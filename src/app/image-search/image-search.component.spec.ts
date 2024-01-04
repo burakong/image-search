9d55216503f01301061962711d34f2ab17db845b
burakong <burakong44@outlook.de>
2024-01-04 11:43:06 +0100

    implement working unit tests

--- src/app/image-search/image-search.component.spec.ts
+++ src/app/image-search/image-search.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ImageSearchComponent } from './image-search.component';
 
 describe('ImageSearchComponent', () => {
@@ -8,9 +9,10 @@ describe('ImageSearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ImageSearchComponent ]
+      declarations: [ImageSearchComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
